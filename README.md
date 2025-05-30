39d9d9f758c55669f63c82ff6e96bc760334e31b
caoyang <1426579967@qq.com>
2025-05-30 15:33:46 +0800

    2025/5/30 first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+point-chat
+
+本项目为点聊聊天项目，包括前端point-chat-web和后端point-chat。
+
+* 技术栈
+  * 前端：vue3, ElementPlus, WebSocket, Typescript
+  * 后端：Springboot, mysql, redis, websocket, OSS
+
+* 功能
+
+  * 用户单聊、群聊
+  * 创建群聊
+  * 搜索好友
+  * 添加好友
+  * 修改信息
+  * 发送文字、图片、文件
+  * AI回复
+
+  
+
+
+
+
+
---
+++ point-chat/point-admin/src/main/java/com/point/chat/pointadmin/service/impl/ChatUserBlacklistServiceImpl.java
@@ -0,0 +1,20 @@
+package com.point.chat.pointadmin.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.point.chat.pointadmin.dao.ChatUserBlacklistDao;
+import com.point.chat.pointadmin.service.ChatUserBlacklistService;
+import com.point.chat.pointcommon.model.ChatUserBlacklist;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * 聊天室用户黑名单表服务接口实现
+ * 
+ * @author Dao-yang
+ * @date: 2024-01-10 09:56:44
+ */
+@Slf4j
+@Service
+public class ChatUserBlacklistServiceImpl extends ServiceImpl<ChatUserBlacklistDao, ChatUserBlacklist> implements ChatUserBlacklistService {
+
+}
